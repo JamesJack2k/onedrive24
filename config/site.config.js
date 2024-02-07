8dd9514074098bf9d8c254af21db5c7571a214d0
JamesJack2k <129104087+JamesJack2k@users.noreply.github.com>
2024-02-07 18:20:01 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
       //name: 'GitHub',
       //link: 'https://github.com/',
     //},
-  ],
+   //],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
